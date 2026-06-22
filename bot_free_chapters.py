fe6e2d5764dfd8e21fe2300c259ea09c74f8a668
Cannibal Turtle <lostbunny98@gmail.com>
2026-06-23 00:00:40 +0800

    Update bot_free_chapters.py

--- bot_free_chapters.py
+++ bot_free_chapters.py
@@ -251,7 +251,7 @@ async def send_new_entries():
                 "chapter_author_url": AUTHOR_URL,
             })
             
-            payload = render_message("free_chapter", ctx)
+            payload = render_message("free_chapters", ctx)
             kwargs = to_discord_py_kwargs(payload)
             
             await dest.send(**kwargs)
